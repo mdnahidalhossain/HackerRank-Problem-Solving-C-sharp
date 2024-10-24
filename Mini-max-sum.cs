8e0d47fbd3731c0655f352111720d73db21f26b6
Md. Nahid Al Hossain <mdnahidalhossain14@gmail.com>
2024-10-24 16:00:21 +0600

    eighth commit

---
+++ Mini-max-sum.cs
@@ -0,0 +1,52 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {   
+        long sum = 0;
+        arr.Sort();
+        
+        
+        long min = arr[0];
+        long max = arr[4];
+        
+        for (int i=0; i<arr.Count; i++) {
+            
+            sum += arr[i]; 
+            
+        }
+        Console.WriteLine($"{sum - max} {sum-min}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
